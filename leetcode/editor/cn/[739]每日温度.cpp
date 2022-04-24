93d6679095d585e76021a1d68f9bf86f372ff06f
keafmd <674155030@qq.com>
2022-04-24 15:37:36 +0800

    2022年4月24日 每日温度

--- leetcode/editor/cn/[739]每日温度.cpp
+++ leetcode/editor/cn/[739]每日温度.cpp
@@ -1,4 +1,5 @@
-//给定一个整数数组 temperatures ，表示每天的温度，返回一个数组 answer ，其中 answer[i] 是指在第 i 天之后，才会有更高的温度
+//给定一个整数数组 temperatures ，表示每天的温度，返回一个数组 answer ，其中 answer[i]
+// 是指在第 i 天之后，才会有更高的温度
 //。如果气温在这之后都不会升高，请在该位置用 0 来代替。 
 //
 // 
@@ -34,14 +35,42 @@
 // Related Topics 栈 数组 单调栈 
 // 👍 1119 👎 0
 
-#include <iostream>
-#include <vector>
+#include<bits/stdc++.h>
 using namespace std;
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-
+        int ans[temperatures.size()];
+        //初始化ans
+        for(int i=0;i<temperatures.size();i++){
+            ans[i]=0;
+        }
+        //初始化单调栈
+        stack<int> st;
+        //遍历temperatures
+        for (int i = 0; i < temperatures.size(); ++i) {
+            //如果单调栈为空，或者当前温度比栈顶元素大，则入栈
+            while (!st.empty() && temperatures[st.top()] < temperatures[i]) {
+                //计算ans
+                ans[st.top()] = i - st.top();
+                //出栈
+                st.pop();
+            }
+            //入栈
+            st.push(i);
+        }
+        //返回ans
+        return vector<int>(ans, ans + temperatures.size());
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
+int main(){
+    Solution solution;
+    vector<int> temperatures = {73,74,75,71,69,72,76,73};
+    vector<int> ans = solution.dailyTemperatures(temperatures);
+    for(int i=0;i<ans.size();i++){
+        cout<<ans[i]<<" ";
+    }
+    return 0;
+}
